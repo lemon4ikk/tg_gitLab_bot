c21a676af3fd3306cf75a91c43eb601c8716858b
Никита Сергеев <nikit.sambo@gmail.com>
2025-06-29 14:12:11 +0300

    add webhook

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ import (
 	"log"
 	"net/http"
 	"net/url"
-	//tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -28,6 +29,26 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	bot, err := tgbotapi.NewBotAPI("7653357171:AAH0irLiLcA8TvZTXHNY3f_pCZMzVPMN0_E")
+	chatID := int64(414747434) // ID бота
+	//chatID := int64(-1002561300903) // ID тестового чата
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	count := 0
+	for _, value := range values {
+		m := value[count]
+
+		msg := tgbotapi.NewMessage(chatID, m)
+		bot.Send(msg)
+		count++
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(values)
@@ -35,7 +56,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/gitLabBot", handler)
+
+	mux.HandleFunc("POST /gitLabBot", handler)
 
 	server := &http.Server{
 		Addr:    ":8080",
